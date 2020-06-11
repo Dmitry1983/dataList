19cacc93e0e397995796761df5be0918973b0f7a
Dmitry1983 <barbash.dv@gmail.com>
2020-06-11 14:36:53 +0300

    rest, add

--- App.js
+++ App.js
@@ -1,35 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
 	View,
 	Text,
 	StyleSheet,
 	FlatList,
 	TouchableOpacity,
+	Alert,
+	TextInput,
+	SafeAreaView,
 } from 'react-native'
 import { data } from './src/data'
 
-const Item = ({ name, text }) => {
-	return (
-		<TouchableOpacity
-			style={styles.item}
-			onPress={() => console.log(text)}
-			activeOpacity={0.3}
-		>
-			<Text style={styles.title}>{name}</Text>
-			{console.log(name)}
-		</TouchableOpacity>
-	)
-}
-
 const App = () => {
+	const [value, setValue] = useState('')
+	const [inputData, setInputData] = useState(data)
+
+	//useEffect(() => {}, [inputData])
+
+	const Item = ({ name, text, id }) => {
+		return (
+			<TouchableOpacity
+				style={styles.item}
+				// onPress={() => console.log(text)}
+				onPress={() => Alert.alert(text, id)}
+				onLongPress={() => {
+					removeItem(id)
+				}}
+				activeOpacity={0.3}
+			>
+				<Text style={styles.title}>{name}</Text>
+				{/* {console.log(name)} */}
+			</TouchableOpacity>
+		)
+	}
+
+	const addItem = () => {
+		if (value.trim()) {
+			const data = [
+				...inputData,
+				{
+					id: Date.now().toString(),
+					title: value,
+					text: (value + ' ').repeat(5),
+				},
+			]
+			setInputData(data)
+			setValue('')
+		} else {
+			Alert.alert('Ошибка ввода')
+		}
+	}
+
+	const removeItem = (id) => {
+		const data = inputData.filter((element) => element.id !== id)
+		setInputData(data)
+		//console.log(data)
+	}
+
+	const editItem = (id) => {}
+
 	return (
-		<View style={styles.container}>
-			<FlatList
-				data={data}
-				renderItem={({ item }) => <Item name={item.title} text={item.text} />}
-				keyExtractor={(item) => item.id}
-			/>
-		</View>
+		<SafeAreaView>
+			<View style={styles.viewAddLine}>
+				<TextInput
+					style={styles.input}
+					value={value}
+					onChangeText={setValue}
+					placeholder="Введите название ..."
+					autoCorrect={false}
+					autoCapitalize="none"
+				/>
+				<TouchableOpacity style={styles.btnAdd} onPress={addItem}>
+					<Text style={styles.title}>ADD</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.container}>
+				<FlatList
+					data={inputData}
+					renderItem={({ item }) => (
+						<Item name={item.title} text={item.text} id={item.id} />
+					)}
+					keyExtractor={(item) => item.id}
+				/>
+			</View>
+		</SafeAreaView>
 	)
 }
 
@@ -37,8 +92,9 @@ export default App
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+		//flex: 1,
 		marginTop: 8,
+		marginBottom: 80,
 	},
 	item: {
 		backgroundColor: '#3498DB',
@@ -51,5 +107,27 @@ const styles = StyleSheet.create({
 		fontSize: 26,
 		color: 'white',
 		textAlign: 'center',
+	},
+	input: {
+		width: '70%',
+		fontSize: 20,
+		marginLeft: 16,
+		borderWidth: 0.25,
+		borderColor: 'black',
+		borderRadius: 16,
+		paddingHorizontal: 16,
+	},
+	btnAdd: {
+		backgroundColor: '#3498DB',
+		borderRadius: 16,
+		padding: 13,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 16,
+	},
+	viewAddLine: {
+		flexDirection: 'row',
+		marginTop: 20,
+		justifyContent: 'space-between',
 	},
 })
--- src/data.js
+++ src/data.js
@@ -2,4 +2,5 @@ export const data = [
 	{ id: '1', title: 'Line 1', text: 'Line 1 line 1 Line 1 line 1' },
 	{ id: '2', title: 'Line 2', text: 'Line 2 line 2 Line 2 line 2' },
 	{ id: '3', title: 'Line 3', text: 'Line 3 line 3 Line 3 line 3' },
+	{ id: '4', title: 'Line 4', text: 'Line 4 line 4 Line 4 line 4' },
 ]
